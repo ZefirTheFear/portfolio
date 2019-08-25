9e5f168326959e94f8548e18c6d2d54e8ddf5e22
ZefirTheFear <z4clr07.gaming@gmail.com>
2019-08-25 23:43:15 +0300

    Contacts

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,8 +3,19 @@ import "./Contact.scss";
 
 const Contact = () => {
   return (
-    <div>
-      <div>Contact</div>
+    <div className="contact">
+      <h1 className="contact__lg-heading">
+        Contact <span className="contact__text-secondary">Me</span>
+      </h1>
+      <h2 className="contact__sm-heading">This is how you can reach me...</h2>
+      <div className="contact__contacts">
+        <div className="contact__contact">
+          <span className="contact__text-secondary">Email:</span> johndoe@test.test
+        </div>
+        <div className="contact__contact">
+          <span className="contact__text-secondary">Phone:</span> (555) 555-5555
+        </div>
+      </div>
     </div>
   );
 };
