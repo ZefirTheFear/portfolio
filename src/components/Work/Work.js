f704630aaa2200fc245dca2660f648c61fa29b2c
ZefirTheFear <z4clr07.gaming@gmail.com>
2019-08-25 22:53:45 +0300

    My work

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -3,8 +3,78 @@ import "./Work.scss";
 
 const Work = () => {
   return (
-    <div>
-      <div>Work</div>
+    <div className="work">
+      <h1 className="work__lg-heading">
+        My <span className="work__text-secondary">Work</span>
+      </h1>
+      <h2 className="work__sm-heading">Check out some of my projects...</h2>
+      <div className="work__projects">
+        <div className="work__project">
+          <img
+            className="work__project-img"
+            src={require("../../assets/img/projects/project1.jpg")}
+            alt="project1"
+          />
+          <a href="#!" className="work__project-link">
+            Project
+          </a>
+          <a href="#!" className="work__project-github-link">
+            Github
+          </a>
+        </div>
+        <div className="work__project">
+          <img
+            className="work__project-img"
+            src={require("../../assets/img/projects/project2.jpg")}
+            alt="project1"
+          />
+          <a href="#!" className="work__project-link">
+            Project
+          </a>
+          <a href="#!" className="work__project-github-link">
+            Github
+          </a>
+        </div>
+        <div className="work__project">
+          <img
+            className="work__project-img"
+            src={require("../../assets/img/projects/project3.jpg")}
+            alt="project1"
+          />
+          <a href="#!" className="work__project-link">
+            Project
+          </a>
+          <a href="#!" className="work__project-github-link">
+            Github
+          </a>
+        </div>
+        <div className="work__project">
+          <img
+            className="work__project-img"
+            src={require("../../assets/img/projects/project4.jpg")}
+            alt="project1"
+          />
+          <a href="#!" className="work__project-link">
+            Project
+          </a>
+          <a href="#!" className="work__project-github-link">
+            Github
+          </a>
+        </div>
+        <div className="work__project">
+          <img
+            className="work__project-img"
+            src={require("../../assets/img/projects/project5.jpg")}
+            alt="project1"
+          />
+          <a href="#!" className="work__project-link">
+            Project
+          </a>
+          <a href="#!" className="work__project-github-link">
+            Github
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
