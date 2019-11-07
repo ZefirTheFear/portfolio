4138b06cf8866cd159462aa5313782210469b864
ZefirTheFear <z4clr07.gaming@gmail.com>
2019-11-07 18:00:10 +0200

    Updates

---
+++ precache-manifest.cf05a2b382609860c6b15e20a9686724.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5eeefadac748f5eada8355bdc7fbf1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7a5ee68523dfb82d2e70",
+    "url": "/portfolio/static/css/main.c5d2cdfa.chunk.css"
+  },
+  {
+    "revision": "b5a8a56be5a660abf2e2",
+    "url": "/portfolio/static/js/2.ab9120bb.chunk.js"
+  },
+  {
+    "revision": "7a5ee68523dfb82d2e70",
+    "url": "/portfolio/static/js/main.029e84d4.chunk.js"
+  },
+  {
+    "revision": "e8aab94e553ef1acd8d3",
+    "url": "/portfolio/static/js/runtime~main.e9a507a1.js"
+  },
+  {
+    "revision": "06596577ae4143d9d51e2184310e23dd",
+    "url": "/portfolio/static/media/Boostrap_logo.06596577.svg"
+  },
+  {
+    "revision": "7415a70abb0fbaeab688508b2d8b1ce4",
+    "url": "/portfolio/static/media/CSS3_logo.7415a70a.svg"
+  },
+  {
+    "revision": "aaec9219d68861e16f79211b86d4df6d",
+    "url": "/portfolio/static/media/Git_logo.aaec9219.svg"
+  },
+  {
+    "revision": "525f317fe9e90927edf7694d88ce42c8",
+    "url": "/portfolio/static/media/HTML5_logo.525f317f.svg"
+  },
+  {
+    "revision": "03d78f02716e2c79fb17e7dc0b455407",
+    "url": "/portfolio/static/media/JQuery_logo.03d78f02.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/portfolio/static/media/JavaScript_logo.dd60b11e.svg"
+  },
+  {
+    "revision": "8f1222a83fffd1b32e57a75354a24f68",
+    "url": "/portfolio/static/media/Mongodb_logo.8f1222a8.svg"
+  },
+  {
+    "revision": "514acb440e800756cb3015521dad85f9",
+    "url": "/portfolio/static/media/Mongoose_logo.514acb44.svg"
+  },
+  {
+    "revision": "89ccb09060a7adf788ea2340c54d6d78",
+    "url": "/portfolio/static/media/Node.js_logo.89ccb090.svg"
+  },
+  {
+    "revision": "8e26f22094a11f6a689d8302dc30782c",
+    "url": "/portfolio/static/media/React_logo.8e26f220.svg"
+  },
+  {
+    "revision": "e0f651ac7aecf9187f66e5bd5f181c10",
+    "url": "/portfolio/static/media/Sass_logo.e0f651ac.svg"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/portfolio/static/media/background.00051dbb.jpg"
+  },
+  {
+    "revision": "ef93b85f91bed41bc96b6a7b7369126c",
+    "url": "/portfolio/static/media/portrait.ef93b85f.jpg"
+  },
+  {
+    "revision": "2efd960d6f072c1970115149dc9607d6",
+    "url": "/portfolio/static/media/project1.2efd960d.jpg"
+  },
+  {
+    "revision": "473d9f55d2f5045e41e3271c338cdb11",
+    "url": "/portfolio/static/media/project2.473d9f55.jpg"
+  },
+  {
+    "revision": "139308d797d4a548920ce783be94237f",
+    "url": "/portfolio/static/media/project3.139308d7.jpg"
+  },
+  {
+    "revision": "6e8cc30d0459394d1b354fe292913564",
+    "url": "/portfolio/static/media/russia.6e8cc30d.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/portfolio/static/media/united-kingdom.e5564902.svg"
+  }
+]);
